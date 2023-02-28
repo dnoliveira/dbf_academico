4c846175518119a1c431b9b2cb28b30ef263befd
Douglas Oliveira <douglas.dno@gmail.com>
2023-02-27 23:31:10 -0300

    Aula-3

--- src/main/java/br/cefet/academico/AcademicoApplication.java
+++ src/main/java/br/cefet/academico/AcademicoApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AcademicoApplication {
 
 	public static void main(String[] args) {
-            
 		SpringApplication.run(AcademicoApplication.class, args);
 	}
 
