b331b5e0050857a59242e235db576b8830e6aab6
Douglas Oliveira <douglas.dno@gmail.com>
2023-06-26 23:01:52 -0300

    Aula-4

--- src/main/java/br/cefet/academico/dao/AlunoDisciplinaDao.java
+++ src/main/java/br/cefet/academico/dao/AlunoDisciplinaDao.java
@@ -11,7 +11,6 @@ import java.util.List;
 import org.jdbi.v3.sqlobject.config.RegisterBeanMapper;
 import org.jdbi.v3.sqlobject.customizer.Bind;
 import org.jdbi.v3.sqlobject.customizer.BindBean;
-import org.jdbi.v3.sqlobject.statement.GetGeneratedKeys;
 import org.jdbi.v3.sqlobject.statement.SqlQuery;
 import org.jdbi.v3.sqlobject.statement.SqlUpdate;
 
